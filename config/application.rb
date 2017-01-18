3ef001795a78c4d06da5d6120f25f836e9d93495
Meredith Jones <meredithjw@gmail.com>
2017-01-18 12:28:41 -0800

    add config.active_record.schema_format line

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
+config.active_record.schema_format = :sql
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
