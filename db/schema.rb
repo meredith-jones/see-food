49dbc32b1baee9d636f69c32df76a58a1dc370aa
Meredith Jones <meredithjw@gmail.com>
2017-01-17 17:44:18 -0800

    create pictures table and tests, migrate pictures table, pass attribute tests, 2 association tests failing

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118012430) do
+ActiveRecord::Schema.define(version: 20170118013659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "pic_url"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["restaurant_id"], name: "index_pictures_on_restaurant_id", using: :btree
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -24,4 +32,5 @@ ActiveRecord::Schema.define(version: 20170118012430) do
     t.datetime "updated_at",   null: false
   end
 
+  add_foreign_key "pictures", "restaurants"
 end
