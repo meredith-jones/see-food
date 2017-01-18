0a3dc9924da51acb97d67e72c7554c76208c2b6a
Meredith Jones <meredithjw@gmail.com>
2017-01-17 21:32:44 -0800

    Add user model and migration through devise token auth. Add tests for likes, add associations and attributes to likes, pass tests for likes.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118013659) do
+ActiveRecord::Schema.define(version: 20170118052446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "picture_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["picture_id"], name: "index_likes_on_picture_id", using: :btree
+    t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
+  end
 
   create_table "pictures", force: :cascade do |t|
     t.string   "pic_url"
@@ -32,5 +41,35 @@ ActiveRecord::Schema.define(version: 20170118013659) do
     t.datetime "updated_at",   null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string   "provider",               default: "email", null: false
+    t.string   "uid",                    default: "",      null: false
+    t.string   "encrypted_password",     default: "",      null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,       null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "email"
+    t.json     "tokens"
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
+    t.index ["email"], name: "index_users_on_email", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true, using: :btree
+  end
+
+  add_foreign_key "likes", "pictures"
+  add_foreign_key "likes", "users"
   add_foreign_key "pictures", "restaurants"
 end
--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "attributes" do
+    it { should have_db_column(:user_id) }
+    it { should have_db_column(:picture_id) }
+  end
+
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:picture) }
+  end
 end
