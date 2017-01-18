09d7783ee5c0ff3e6fc2bcb07d8cde63eea2288b
Meredith Jones <meredithjw@gmail.com>
2017-01-17 17:00:46 -0800

    add tests for restaurant table attributes

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,1 +1,19 @@
 require 'rails_helper'
+
+RSpec.describe Restaurant, type: :model do
+  describe "attributes" do
+    it { should have_db_column(:name) }
+    it { should have_db_column(:address) }
+    it { should have_db_column(:phone_number) }
+    it { should have_db_column(:website) }
+    it { should have_db_column(:created_at) }
+    it { should have_db_column(:updated_at) }
+  end
+
+  describe "associations" do
+    it { should have_many(:pictures) }
+    it { should have_many(:pictures) }
+  end
+
+
+end
