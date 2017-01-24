2e19c41ddb51796d42232a2d646de43f324707fe
tereg <teregalla@gmail.com>
2017-01-24 13:16:08 -0800

    Remove irrelevant tests from users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  it { should route(:get, '/users').to(action: :index) }
-  it { should route(:get, '/posts/1').to(action: :show, id: 1) }
 
 end 
