dc7e021ef346ad686fc9782f1c920e3ce502ecb8
Timothy Tao <timctao@gmail.com>
2017-01-26 14:55:36 -0800

    Fix rspec tests to pass new restaurants table

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,9 +3,8 @@ require 'rails_helper'
 RSpec.describe Restaurant, type: :model do
   describe "attributes" do
     it { should have_db_column(:name) }
-    it { should have_db_column(:address) }
+    it { should have_db_column(:address1) }
     it { should have_db_column(:phone_number) }
-    it { should have_db_column(:website) }
     it { should have_db_column(:created_at) }
     it { should have_db_column(:updated_at) }
   end
