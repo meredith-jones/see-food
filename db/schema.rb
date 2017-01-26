30d9a41a154fd9b7dd26fcde483396e9caeb235c
Timothy Tao <timctao@gmail.com>
2017-01-26 13:42:37 -0800

    Add user reference to pictures table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118052446) do
+ActiveRecord::Schema.define(version: 20170126213932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,9 @@ ActiveRecord::Schema.define(version: 20170118052446) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
     t.index ["restaurant_id"], name: "index_pictures_on_restaurant_id", using: :btree
+    t.index ["user_id"], name: "index_pictures_on_user_id", using: :btree
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -72,4 +74,5 @@ ActiveRecord::Schema.define(version: 20170118052446) do
   add_foreign_key "likes", "pictures"
   add_foreign_key "likes", "users"
   add_foreign_key "pictures", "restaurants"
+  add_foreign_key "pictures", "users"
 end
