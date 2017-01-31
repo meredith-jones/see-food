66bb77319be4a7bbd6dbff20943de33e1f00c55c
Timothy Tao <timctao@gmail.com>
2017-01-31 15:38:11 -0800

    Remove through command for picture-user association

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :restaurant
-  # revisit this, not sure if syntax is correct
-  belongs_to :user, through: :likes
+  # revisit this, not sure if syntax is correct; pics belongs to user??
+  belongs_to :user
   has_many :likes
 end
