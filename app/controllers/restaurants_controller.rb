c7ec7dc74d6e26f108c36e4f80171c51f3eada52
Meredith Jones <meredithjw@gmail.com>
2017-01-31 16:51:50 -0800

    Add functionality to instantiate a restaurant.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,12 +9,20 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    restaurant = Restaurant.new(restaurant_params)
+    restaurant.save
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :image_url, :phone_number, :address1, :address2, :city, :state, :zip_code, :longitude, :latitude)
   end
 
 end
