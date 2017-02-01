bbeb12e77d9ad3106c4c9268c762b20671b8d6fc
Meredith Jones <meredithjw@gmail.com>
2017-01-31 17:52:57 -0800

    add functionality to instantiate a picture and a like

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,8 +9,13 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    restaurant = Restaurant.new(restaurant_params)
-    restaurant.save
+    # thinking we should intantiate all these things here, because pictures and likes depend on restaurants:
+    # restaurant = Restaurant.new(restaurant_params)
+    # restaurant.save
+    # picture = Picture.new(restaurant_id: restaurant.id, pic_url: restaurant_params.image_url, user_id: current_user.id)
+    # picture.save
+    # like = Like.new(picture_id: picture.id, user_id: current_user.id)
+    # like.save
   end
 
   def update
